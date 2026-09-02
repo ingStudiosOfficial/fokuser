6aa0d27200add05932f360d5e9087aee5da93887
Ethan Lee Qi Yang <ingstudiosofficial@gmail.com>
2026-09-02 19:48:07 +0800

    feat: create alarms for focus schedule

--- extension/src/utils/schedule.ts
+++ extension/src/utils/schedule.ts
@@ -8,3 +8,17 @@ export async function getSchedule(): Promise<ScheduleData[]> {
 	const { schedule } = await chrome.storage.local.get('schedule');
 	return schedule || [];
 }
+
+export async function registerSchedule(schedule: ScheduleData[]) {
+	for (const item of schedule) {
+		await chrome.alarms.create(`start-focus-${item.key}`, {
+			when: item.startTime,
+			persistAcrossSessions: true,
+		});
+
+		await chrome.alarms.create(`end-focus-${item.key}`, {
+			when: item.endTime,
+			persistAcrossSessions: true,
+		});
+	}
+}
